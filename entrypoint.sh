e729fe771ede5bf5a130c1fac715b0fc23b3400c
Muhammad Zain <zainuet2022@gmail.com>
2026-08-16 03:30:50 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
+cd /var/www/html/devops-assesments/app/
 # Cache configuration, routes, and views
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
 # Run database migrations automatically
-cd /var/www/html/devops-assesments/app/
+
 php artisan migrate --force
 
 # Start Apache in foreground
