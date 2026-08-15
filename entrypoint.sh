7abb927961fd64b2c771716223b054615c5e314d
Muhammad Zain <zainuet2022@gmail.com>
2026-08-16 03:24:03 +0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Cache configuration, routes, and views
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Run database migrations automatically
+php artisan migrate --force
+
+# Start Apache in foreground
+exec apache2-foreground
